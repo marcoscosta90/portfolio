5236e0122a2426ed9cd3c96cbb074cb57e41a48a
marcoscosta90 <marcoscosta.dev@gmail.com>
2021-01-07 09:49:01 -0300

    portfolio completed

--- script.js
+++ script.js
@@ -30,5 +30,12 @@ $(document).ready(function(){
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
+    });
+
+    var typed = new Typed('.typing-2', {
+        strings: ["Desenvolvedor Web", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     })
 })
